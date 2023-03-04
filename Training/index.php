b5a5fecfe7c5a9bc75cf756d442b7d59620233ea
Marko Mikesic <marko.mikesicgit@gmail.com>
2023-03-04 09:12:12 +0000

    vježbe PHP

--- Training/index.php
+++ Training/index.php
@@ -29,4 +29,27 @@ try {
 
 }
 
+?>
+
+// use echo to print all three functions
+
+<?php
+function first()
+{
+  return "You did it!\n";
+}
+
+function second()
+{
+  return "You're amazing!\n";
+}
+
+function third()
+{
+  return "You're a coding hero!\n";
+}
+
+echo first(), second(), third();
+?>
+
 
